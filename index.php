522bc0da633c5987b56ce7182d895b40ee837672
Nicole Marinho Espindola <nicole.m.espindola@gmail.com>
2023-06-02 14:43:19 -0300

    adicionando about us e meet us

---
+++ index.php
@@ -0,0 +1,124 @@
+<?php
+    include_once('header.html');
+?>
+
+<head>
+    <script
+    src="https://kit.fontawesome.com/e02aa7a64f.js"
+    crossorigin="anonymous"
+  ></script>
+  <link
+    rel="stylesheet"
+    href="https://fontawesome.com/icons/basket-shopping?f=classic&s=solid"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fontawesome.com/icons/magnifying-glass?f=classic&s=solid"
+  />
+  <link rel="stylesheet" href="./css/header.css" />
+  <link rel="stylesheet" href="./css/home.css">
+</head>
+
+<section class="about-us">
+    <div class="email">
+        <div class="nav-header">
+            <span class="new-msg">New messege</span>
+            <div class="icons">
+                <i class="fa-solid fa-window-minimize" style="color: #998FD0;"></i>
+                <i class="fa-thin fa-window-restore" style="color: #998FD0;"></i>
+                <i class="fa-solid fa-xmark" style="color: #998FD0;"></i>
+            </div>
+        </div>
+        <div class="email-info">
+            <div class="underline-block">
+                <span class="grey-text">To:<span class="purple-text">You</span></span>
+            </div>
+            <div class="underline-block margin">
+                <span class="grey-text">Subject: <span class="purple-text">Who we are</span></span>
+            </div>
+        </div>
+        <div class="email-body">
+            <span class="main-info">
+                We at CAFFEINE MANIA value the quality, technology and flavor of our drinks
+                and that is why we have serious and committed partnerships to offer the best
+                of experiences to our consumers.
+            </span>
+            <div class="bye-msg">
+                <span class="sub-info">We hope you well,</span>
+                <span class="end-info">CAFFEINE MANIA, xoxo</span>
+            </div>
+        </div>
+        <div class="email-footer">
+            <button class="btn btn-size">Send</button>
+            <div class="email-logo">
+                <span class="purple-text">catmail</span>
+                <img class="logo-email" src="./img/logo-icon-sem fundo.png" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="cat">
+        <img class="cat-size"src="./img/cat-boba.png" alt="Cat drinking bubble tea">
+    </div>
+</section>
+<section class="meet-us">
+    <div class="main-title">
+        <h1 class="title">meet us in our stores</h1>
+        <i class="fa-solid fa-right-long" style="color: #B9004E;"></i>
+    </div>
+    <div class="container">
+        <div class="child child-purple">
+            <span>
+                SHOPPING PENHA
+                R. DR. JOÃO RIBEIRO, 304
+                SÃO PAULO</span>
+        </div>
+        <div class="child child-pink">
+            <span>
+                SHOPPING PENHA
+                R. DR. JOÃO RIBEIRO, 304
+                SÃO PAULO
+            </span>
+        </div>
+        <div class="child child-purple">
+            <span>
+                SHOPPING PENHA
+                R. DR. JOÃO RIBEIRO, 304
+                SÃO PAULO</span>
+        </div>
+        <div class="child child-pink">
+            <span>
+                SHOPPING PENHA
+                R. DR. JOÃO RIBEIRO, 304
+                SÃO PAULO
+            </span>
+        </div>
+        <div class="child child-purple">
+            <span>
+                SHOPPING PENHA
+                R. DR. JOÃO RIBEIRO, 304
+                SÃO PAULO</span>
+        </div>
+        <div class="child child-pink">
+            <span>
+                SHOPPING PENHA
+                R. DR. JOÃO RIBEIRO, 304
+                SÃO PAULO
+            </span>
+        </div>
+        <div class="child child-purple">
+            <span>
+                SHOPPING PENHA
+                R. DR. JOÃO RIBEIRO, 304
+                SÃO PAULO</span>
+        </div>
+        <div class="child child-pink">
+            <span>
+                SHOPPING PENHA
+                R. DR. JOÃO RIBEIRO, 304
+                SÃO PAULO
+            </span>
+        </div>
+    </div>
+</section>
+</body>
+</html>
